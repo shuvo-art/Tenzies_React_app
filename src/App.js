0ef60c5d5a01b3cee503f2271fdbbe1bc9404af3
shuvo-art <shuvo83qn@gmail.com>
2024-01-21 17:24:40 +0600

    Hold dice Element

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import React from "react";
+import { nanoid } from "nanoid";
 import Die from "./component/Die";
+
 function App() {
     const [dice, setDice] = React.useState(allNewDice())
 
     function allNewDice() {
         const newDice = [];
         for (let i = 0; i < 10; i++) {
-            newDice.push(Math.floor(Math.random() *6) +1);
+            newDice.push({
+                value: Math.floor(Math.random() * 6) + 1,
+                isHeld: false, 
+                id: nanoid()
+            });
         }
         return newDice;
     }
 
-    const diceElements = dice.map(die => <Die value={die} />);
+    function toggleIsHeld(id) {
+        setDice(prevDice => prevDice.map(die => {
+            return die.id === id ? 
+            {...die, isHeld: !die.isHeld} : 
+            die
+        })
+    )}
+
+    const diceElements = dice.map(die => (<Die key={die.id} value={die.value} isHeld={die.isHeld} onHandle={() => toggleIsHeld(die.id)} />));
 
   return (
     <main>
--- src/component/Die.js
+++ src/component/Die.js
@@ -1,8 +1,11 @@
 import React from "react";
 
 export default function Die(props) {
+    const styles = {
+        backgroundColor: props.isHeld ? "green" : "white"
+    }
     return(
-        <div className="die-face">
+        <div className="die-face" style={styles} onClick={props.onHandle}>
             <h2 className="die-num">{props.value}</h2>
         </div>
     )
